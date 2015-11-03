66c39b23d84ebc7852d2a2b7320350afa3a75d07
easonlin MacBook Air <easonlin404@gmail.com>
2015-11-03 23:22:24 +0800

    fix

--- src/main/java/com/eason/jersey/ClientExample.java
+++ src/main/java/com/eason/jersey/ClientExample.java
@@ -2,19 +2,24 @@ package com.eason.jersey;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 public class ClientExample {
 
 	public static void main(String[] args) {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/jersey-server-jackson/api/v1.0/").path(
-				"hello");
+		WebTarget target = client.target("http://localhost:8080/jersey-server-jackson/api/v1.0/")
+				.register(JacksonFeature.class).path("hello");
 
+		User user = new User("Carry", "hello");
 
-		Response response  = target.request().get();
-
+		Response response = target.request()
+				.post(Entity.entity(user, MediaType.APPLICATION_JSON));
 
 		System.out.println("status:" + response.getStatus());
 		System.out.println(response.readEntity(String.class));
